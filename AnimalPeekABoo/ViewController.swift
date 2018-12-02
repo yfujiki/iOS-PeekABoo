59d190f462efa9a7cbb24892b60e77e99a619156
Yuichi Fujiki <yuichi@chaione.com>
2018-12-02 14:09:17 -0700

    Initial Commit

---
+++ AnimalPeekABoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimalPeekABoo
+//
+//  Created by Yuichi Fujiki on 12/2/18.
+//  Copyright © 2018 Yuichi Fujiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
